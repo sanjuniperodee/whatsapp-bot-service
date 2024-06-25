e9149ce36fc29eb04c2261560efaaaf17a1cc26e
livinbackend <bekhanbackend@livin.kz>
2024-06-25 10:50:44 +0500

    туц

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,6 @@ import { WhatsAppModule } from "@modules/whatsapp/whatsapp.module";
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
       inject: [ConfigService],
     }),
     WhatsAppModule,
