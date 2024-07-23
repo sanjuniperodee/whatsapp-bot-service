2744c27c3736a71b71fd52cb88fbfef1036772b8
livinbackend <bekhanbackend@livin.kz>
2024-07-24 00:17:37 +0500

    main

--- src/modules/whatsapp/whatsapp.controller.ts
+++ src/modules/whatsapp/whatsapp.controller.ts
@@ -7,7 +7,7 @@ export class WhatsAppController {
 
   @Post('handle-message')
   async handleIncomingMessage(@Body() input: any){
-    Logger.log(input)
+    Logger.log(input.instanceData.wid.split('@')[0])
     return this.whatsappService.handleIncomingMessage(input)
   }
 
