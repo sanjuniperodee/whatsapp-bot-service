3448debe4086402af0cda728d0274aaa4febb659
livinbackend <bekhanbackend@livin.kz>
2024-08-01 11:21:42 +0500

    main

---
+++ database/migrations/20240724170416_create_whatsapp_user_table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from 'knex';
+
+const tableName = 'whatsapp_users';
+
+export async function up(knex: Knex) {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  return knex.schema.createTableIfNotExists(tableName, async (t) => {
+    t.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+    t.string('phone', 16).unique().index();
+    t.string('name', 255).notNullable();
+    t.timestamp('createdAt').defaultTo(knex.fn.now());
+    t.timestamp('updatedAt').defaultTo(knex.fn.now());
+    t.timestamp('deletedAt');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTableIfExists(tableName);
+}
