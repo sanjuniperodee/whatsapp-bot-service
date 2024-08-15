ecc5b30e09668ab320d5cde5280cdbdb1a790dce
livinbackend <bekhanbackend@livin.kz>
2024-08-15 23:22:10 +0500

    main

--- src/taxi-context/domains/order-request/services/create-order/create-order-request.ts
+++ src/taxi-context/domains/order-request/services/create-order/create-order-request.ts
@@ -10,44 +10,53 @@ import {
 } from 'class-validator';
 import { OrderType } from '@infrastructure/enums';
 import { IsNull } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderRequest {
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsString()
   readonly phone: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsString()
   readonly from: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsString()
   readonly to: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsLongitude()
   readonly lng: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsLatitude()
   readonly lat: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsNumber()
   readonly price: number;
 
+  @ApiProperty()
   @IsString()
   @IsDefined()
   @IsNotEmpty()
   @IsEnum(OrderType)
   readonly orderType: OrderType;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsString()
--- src/taxi-context/domains/order-request/services/make-review/create-order-request.ts
+++ src/taxi-context/domains/order-request/services/make-review/create-order-request.ts
@@ -1,16 +1,21 @@
 import { IsDefined, IsNotEmpty, IsNumber, IsString, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class MakeReviewRequest {
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsUUID('4')
   readonly orderRequestId: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsString()
   readonly comment: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDefined()
   @IsNumber()
