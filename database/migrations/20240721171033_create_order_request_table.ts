46742712a2a64bce05aa1a17fc5916085044f281
livinbackend <bekhanbackend@livin.kz>
2024-08-21 19:29:48 +0500

    main

--- database/migrations/20240721171033_create_order_request_table.ts
+++ database/migrations/20240721171033_create_order_request_table.ts
@@ -9,7 +9,6 @@ export async function up(knex: Knex) {
   return knex.schema.createTableIfNotExists(tableName, async (t) => {
     t.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
     t.uuid('driverId').index().notNullable().references('id').inTable('users').onDelete('cascade').onUpdate('cascade');
-    t.enum('orderType', orderType).notNullable();
     t.enum('orderType', orderType).notNullable().defaultTo('CREATED');
     t.string('sessionId', 16).index();
     t.string('from', 255).notNullable();
