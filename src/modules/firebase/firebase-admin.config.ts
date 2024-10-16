4fe3eab4dd557df3d5135b09e843a498b11197a3
livinbackend <bekhanbackend@livin.kz>
2024-10-16 22:18:20 +0500

    main

--- src/modules/firebase/firebase-admin.config.ts
+++ src/modules/firebase/firebase-admin.config.ts
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
 
 admin.initializeApp({
-  credential: admin.credential.cert('src/modules/firebase/aktau-go-firebase-adminsdk-yairb-1a4fff2d03.json'),
+  credential: admin.credential.cert('src/modules/firebase/aktau-go-firebase-adminsdk-yairb-1b4b0b54cc.json'),
 });
 
 export default admin;
