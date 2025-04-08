676538e60816da7d2027c7590e4ce715ced9bdfb
livinbackend <bekhanbackend@livin.kz>
2025-04-08 14:46:44 +0500

    main

--- src/modules/firebase/firebase-admin.config.ts
+++ src/modules/firebase/firebase-admin.config.ts
@@ -1,10 +1,7 @@
-import * as path from 'path';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    path.join(__dirname, 'aktau-go-firebase-adminsdk-yairb-1b4b0b54cc.json')
-  ),
+  credential: admin.credential.cert('src/modules/firebase/aktau-go-firebase-adminsdk-yairb-1b4b0b54cc.json'),
 });
 
 export default admin;
