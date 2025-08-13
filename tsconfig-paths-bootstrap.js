42ed876a3384be6a247a032d4f5ac5aeed2db437
Bekkhan <87779571856b@gmail.com>
2025-08-13 11:18:02 +0500

    trying clustering

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -8,7 +8,7 @@ const baseUrl = './';
 // Adjust paths for compiled output (replace src with dist)
 const adjustedPaths = {};
 for (const [key, value] of Object.entries(tsConfig.compilerOptions.paths)) {
-  adjustedPaths[key] = value.map(path => path.replace('./src/', './dist/'));
+  adjustedPaths[key] = value.map(path => path.replace('src/', 'dist/'));
 }
 
 // Add direct src/* mapping to dist/*
